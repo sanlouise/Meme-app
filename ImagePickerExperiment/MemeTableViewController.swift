5f5ef35835666e22b935027c5b5a8a9be438fd4c
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-03-20 18:03:57 +0100

    Update Table View Controller

--- ImagePickerExperiment/MemeTableViewController.swift
+++ ImagePickerExperiment/MemeTableViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class MemeTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    
+    @IBOutlet weak var tableImage: UIImageView!
+    @IBOutlet weak var memeTextLabel: UILabel!
     var memes: [Meme]!
     
     override func viewWillAppear(animated: Bool) {
@@ -27,42 +30,45 @@ class MemeTableViewController: UIViewController, UITableViewDataSource, UITableV
         super.viewDidAppear(true)
         
     }
-    
 
-    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return memes.count
     }
     
+    @IBAction func addMeme(sender: AnyObject) {
+        
+        let object: AnyObject = storyboard!.instantiateViewControllerWithIdentifier("EditMemeViewController")
+        let createMemeViewController = object as! EditMemeViewController
+        presentViewController(createMemeViewController, animated: true, completion: nil)
+    }
+    
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("memeTableCell")!
+        let cell = tableView.dequeueReusableCellWithIdentifier("memeTableCell", forIndexPath: indexPath)
         let meme = memes[indexPath.row]
-        cell.imageView?.image = meme.memedImage
+        tableImage.image = meme.originalImage
+        memeTextLabel.text = "\(meme.topTextField) \(meme.bottomTextField)"
         return cell
     }
     
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    // After selecting a meme, show detail view controller.
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let detailController = storyboard!.instantiateViewControllerWithIdentifier("SentMemeDetailViewController") as! SentMemeDetailViewController
         detailController.meme = memes[indexPath.row]
         navigationController!.pushViewController(detailController, animated: true)
     }
     
+    //     Swiping to the left should delete the list item.
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if (editingStyle == UITableViewCellEditingStyle.Delete) {
             (UIApplication.sharedApplication().delegate as! AppDelegate).memes.removeAtIndex(indexPath.row)
             tableView.reloadData()
         }
     }
-    
-//     Swiping to the left should delete the list item.
-    
-//    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-//        if editingStyle == UITableViewCellEditingStyle.Delete {
-//            tableView.removeAtIndex(indexPath.row)
-//
-//        }
-//        
-//    }
 
     
     
